f5cd82aaa07249ec97afc056a314a84efb217b52
CrossBanaha <stephano.carbajal@ucsm.edu.pe>
2024-08-23 11:16:00 -0500

    songs_table.php: This table manages a url, an element that can be null, date, duration, name and 2 foreign keys, one from the author table and the other from the genre table

---
+++ database/migrations/2024_08_08_122028_create_songs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->string('url');
+            $table->string('title',30);
+            $table->string('description',60)->nullable();
+            $table->date('premiere');
+            $table->time('duration',3);
+            $table->foreignId('author_id')->constrained();
+            $table->foreignId('genre_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('songs');
+    }
+};
