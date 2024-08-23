190271d4bc1d94f5b9da6559236a7285c77b0b2d
CrossBanaha <stephano.carbajal@ucsm.edu.pe>
2024-08-23 11:40:05 -0500

    app.blade.php: open the html document and use the @vite, @include calls to name and use other views

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,17 @@
+<!-- resources/views/app.blade.php -->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @vite('resources/scss/app.scss')
+        <title>@yield('title', 'Spotify')</title>
+    </head>
+    <body>
+        <div class="flex">
+            @include('layout.sidebar')
+            @yield('content')
+        </div>s
+    </body>
+</html>
